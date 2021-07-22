4c6b15e34600167307b1f5f30435f4ae0dc021cb
Lailson Henrique <lailson.pi@gmail.com>
2021-07-22 11:13:26 -0300

    criado os componentes da tela inicial (titulo, input, botao de calcular e resultado)

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import { Text, View } from 'react-native';
+
+const Tela = styled.View`
+  flex: 1;
+  align-items: center;
+`
+
+const Titulo = styled.Text`
+  font-size: 25px;
+  margin-top: 15px;
+`
+
+const Input = styled.TextInput`
+  width: 90%;
+  background-color: #c4c4c4;
+  height: 50px;
+  font-size: 25px;
+  margin-top: 20px;
+  padding-left: 15px;
+  border-radius: 15px;
+`
+
+const CalcularBotao = styled.Button``
+
+const CalcularView = styled.View`
+  margin-top: 50px;
+`
+
+const ResultadoView = styled.View`
+  background-color: green;
+  margin-top: 50px;
+  padding: 24px;
+  align-items: center;
+  border-radius: 10px;
+`
+
+const ResultadoTexto = styled.Text`
+  font-size: 20px;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Projeto inicial!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tela>
+      <Titulo>Calculadora de IMC</Titulo>
+      <Input placeholder="Peso" keyboardType="numeric" />
+      <Input placeholder="Altura" keyboardType="numeric" />
+      <CalcularView>
+        <CalcularBotao title="Calcular" onPress={ () => {} } />
+      </CalcularView>
+      <ResultadoView>
+        <ResultadoTexto>30</ResultadoTexto>
+        <ResultadoTexto>Normal</ResultadoTexto>
+      </ResultadoView>
+    </Tela>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
